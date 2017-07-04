db73f490e57957da08d1c5d6c6a0914b95e01393
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-07-03 22:56:23 -0300

    Extracts out ProxyFactory

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,21 +7,19 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            get(target, prop, receiver) {
-                if (['adiciona', 'limpa'].includes(prop) && typeof(target[prop]) === typeof(Function)) {
-                    return function () {
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(...arguments);
-            }
-        });
+
+        this._listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacoes(),
+            ['adiciona', 'limpa'],
+            model => this._negociacoesView.update(model));
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),
+            ['texto'],
+            model => this._mensagemView.update(model));
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagem = new Mensagem();
         this._mensagemView.update(this._mensagem);
     }
 
@@ -29,14 +27,12 @@ class NegociacaoController {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._novaNegociacao());
         this._mensagem.texto = "Negociação adicionada com sucesso!";
-        this._mensagemView.update(this._mensagem);
         this._resetaForm();
     }
 
     limpa() {
         this._listaNegociacoes.limpa();
         this._mensagem.texto = "Negociações apagadas com sucesso!";
-        this._mensagemView.update(this._mensagem);
     }
 
     _novaNegociacao() {
---
+++ client/js/app/services/ProxyFactory.js
@@ -0,0 +1,28 @@
+class ProxyFactory {
+
+    static create(object, props, trap) {
+        return new Proxy(object, {
+            get(target, prop, receiver) {
+                if (props.includes(prop) && ProxyFactory.isFunction(target[prop])) {
+                    return function () {
+                        let result = Reflect.apply(target[prop], target, arguments);
+                        trap(object);
+                        return result;
+                    };
+                }
+                return Reflect.get(...arguments);
+            },
+            set(target, prop, value, receiver) {
+                let result = Reflect.set(...arguments);
+                if (props.includes(prop)) {
+                    trap(target);
+                }
+                return result;
+            }
+        });
+    }
+
+    static isFunction(subject) {
+        return typeof(subject) === typeof(Function);
+    }
+}
